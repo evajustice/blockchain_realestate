8a02b27bbcad5b93e864e3059a75adf36626c854
dcohen38 <128623939+dcohen38@users.noreply.github.com>
2023-04-11 11:57:02 -0400

    Delete PagesStart_Feb directory

--- PagesStart_Feb/Original_Random/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = (ctx) => ({
-	map: ctx.options.map,
-	plugins: {
-		autoprefixer: {},
-		cssnano: ctx.env === "production" ? {} : false,
-		"postcss-import": { root: ctx.file.dirname },
-	},
-});
